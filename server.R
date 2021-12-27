e5b1881f3edf632d3ae6ee18576c4de639a036a7
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2021-12-27 13:05:11 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
+# Read data
+basketball <- read.csv('2021_NBA_Predictions.csv')
+names_sorted <- sort(unique(basketball$Name))
+teams_sorted <-  sort(c(unique(basketball$Team), 'ALL'))
+stats <- c('Points', 'Rebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'Fantasy Score')
+
 ####################################
 # Server                           #
 ####################################
