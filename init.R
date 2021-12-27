ef68f64f5a2cd2bdb3214c038150ce09038c5e5b
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2021-12-27 12:43:57 -0800

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "shinythemes", 'data.table')
+my_packages = c("shiny", "shinythemes", 'data.table', 'dplyr', 'tidyverse')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
